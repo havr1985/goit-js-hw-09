5c098bd1ffb7d91f0437527ea31153fd90bd9747
havr1985 <gavrtel1985@gmail.com>
2023-08-07 09:49:05 +0300

    task 1 finished

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,25 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
 
+startBtn.addEventListener('click', onClickStart);
+stopBtn.addEventListener('click', onClickStop);
+const bodyElement = document.querySelector('body');
+
+stopBtn.disabled = true;
+
+function onClickStart() {
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+    timerID = setInterval(() => {
+        bodyElement.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+}
+function onClickStop() {
+    startBtn.disabled = false;
+    stopBtn.disabled = true;
+    clearInterval(timerID);
+};
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
